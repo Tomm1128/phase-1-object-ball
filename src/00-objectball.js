b8c91a1d857bf211a917ea4d5cc31fc9dc703542
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-05-08 10:48:31 -0400

    Add home team object

--- src/00-objectball.js
+++ src/00-objectball.js
@@ -3,14 +3,72 @@ const gameObject = {
       teamName: "Brooklyn Nets",
       colors: ["black", "white"],
       players: {
-        
+        alanAnderson:{
+          number: 0,
+          shoe: 16,
+          points: 22,
+          rebounds: 12,
+          assists: 12,
+          steals: 3,
+          blocks: 1,
+          slamDunks: 1,
+        },
+        reggiesEvans:{
+          number: 30,
+          shoe: 14,
+          points: 12,
+          rebounds: 12,
+          assists: 12,
+          steals: 12,
+          blocks: 12,
+          slamDunks: 7,
+        },
+        brookLopez:{
+          number: 11,
+          shoe: 17,
+          points: 17,
+          rebounds: 19,
+          assists: 10,
+          steals: 3,
+          blocks: 1,
+          slamDunks: 15,
+        },
+        masonPlumlee:{
+          number: 1,
+          shoe: 19,
+          points: 26,
+          rebounds: 12,
+          assists: 6,
+          steals: 3,
+          blocks: 8,
+          slamDunks: 5,
+        },
+        jasonTerry:{
+          number: 31,
+          shoe: 15,
+          points: 19,
+          rebounds: 2,
+          assists: 2,
+          steals: 4,
+          blocks: 11,
+          slamDunks: 1,
+        }
       }
     },
     away: {
       teamName: "awayTeam",
       colors: ["white", "blue"],
       players: {
+        alanAnderson:{
+          number: 0,
+          shoe: 16,
+          points: 22,
+          rebounds: 12,
+          assists: 12,
+          steals: 3,
+          blocks: 1,
+          slamDunks: 1,
+        }
       }
-    },
-
+    }
 }

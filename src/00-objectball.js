87d4fcb5b48da994ce3b8e063599744a3dc67e9a
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-05-08 10:52:58 -0400

    Add away team object

--- src/00-objectball.js
+++ src/00-objectball.js
@@ -1,7 +1,7 @@
 const gameObject = {
     home: {
       teamName: "Brooklyn Nets",
-      colors: ["black", "white"],
+      colors: ["Black", "White"],
       players: {
         alanAnderson:{
           number: 0,
@@ -56,18 +56,58 @@ const gameObject = {
       }
     },
     away: {
-      teamName: "awayTeam",
-      colors: ["white", "blue"],
+      teamName: "Carlotte Hornets",
+      colors: ["Turquoise", "Purple"],
       players: {
-        alanAnderson:{
+        jeffAdrien:{
+          number: 4,
+          shoe: 18,
+          points: 10,
+          rebounds: 1,
+          assists: 1,
+          steals: 2,
+          blocks: 7,
+          slamDunks: 2,
+        },
+        bismakBiyombo:{
           number: 0,
           shoe: 16,
-          points: 22,
+          points: 12,
+          rebounds: 4,
+          assists: 7,
+          steals: 7,
+          blocks: 15,
+          slamDunks: 10,
+        },
+        desagnaDiop:{
+          number: 2,
+          shoe: 14,
+          points: 24,
           rebounds: 12,
           assists: 12,
-          steals: 3,
+          steals: 4,
+          blocks: 5,
+          slamDunks: 5,
+        },
+        benGordon:{
+          number: 8,
+          shoe: 15,
+          points: 33,
+          rebounds: 3,
+          assists: 2,
+          steals: 1,
           blocks: 1,
-          slamDunks: 1,
+          slamDunks: 0,
+        },
+        brendanHaywood:{
+          number: 33,
+          shoe: 15,
+          points: 6,
+          rebounds: 12,
+          assists: 12,
+          steals: 22,
+          blocks: 5,
+          slamDunks: 12,
         }
       }
     }

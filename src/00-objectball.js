caef940cb2bba499d296c3050ce3518022b3ef23
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-05-08 10:43:49 -0400

    Create game object

---
+++ src/00-objectball.js
@@ -0,0 +1,16 @@
+const gameObject = {
+    home: {
+      teamName: "Brooklyn Nets",
+      colors: ["black", "white"],
+      players: {
+        
+      }
+    },
+    away: {
+      teamName: "awayTeam",
+      colors: ["white", "blue"],
+      players: {
+      }
+    },
+
+}
